6bb66c4841011b15d8c4253a86a257467ae9d97b
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-07-12 14:14:27 +0200

    initing DailyStatusLogs

---
+++ BohemianHealth/Models/DailyStatusLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BohemianHealth.Models
+{
+	public class DailyStatusLog
+	{
+		public int ID { get; set; }
+
+		[DataType(DataType.Date)] // A [DataType] attribute that specifies the type of data in the Date property. With this attribute: The user isn't required to enter time information in the date field, Only the date is displayed, not time information.
+		public DateTime? Date { get; set; }
+		public float Weight { get; set; }
+		public int OxygenSaturation { get; set; }
+		public int Sleep { get; set; }
+		public string? Mood { get; set; }
+		public string? Description { get; set; }
+	}
+}
+
