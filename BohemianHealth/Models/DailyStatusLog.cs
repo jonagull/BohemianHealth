0e117a002759db531241d22c6d12ac184014f5d2
Boien <jonathangullik@gmail.com>
2022-07-13 22:32:34 +0200

    changed type

--- BohemianHealth/Models/DailyStatusLog.cs
+++ BohemianHealth/Models/DailyStatusLog.cs
@@ -9,7 +9,7 @@ namespace BohemianHealth.Models
 
 		[DataType(DataType.Date)] // A [DataType] attribute that specifies the type of data in the Date property. With this attribute: The user isn't required to enter time information in the date field, Only the date is displayed, not time information.
 		public DateTime? Date { get; set; }
-		public float Weight { get; set; }
+		public double Weight { get; set; }
 		public int OxygenSaturation { get; set; }
 		public int Sleep { get; set; }
 		public string? Mood { get; set; }
